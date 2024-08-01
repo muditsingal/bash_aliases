57d97379f8933d53b43732049c6703f0dc611172
Mudit Singal <muditsingal@gmail.com>
2024-08-01 17:04:41 -0400

    Updated docker installation steps

--- ubuntu22_software_install.sh
+++ ubuntu22_software_install.sh
@@ -22,18 +22,8 @@ sudo apt install --install-recommends winehq-stable -y
 sudo apt install git -y
 
 # Install Docker
-sudo apt install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh --dry-run
 
 # Install Neofetch
 sudo apt install neofetch -y
