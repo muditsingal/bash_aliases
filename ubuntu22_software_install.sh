95328671aa3c049ba267cc002566f934ce8b3614
Mudit Singal <muditsingal@gmail.com>
2024-08-02 13:49:40 -0400

    Added ros 2 humble installation commands

--- ubuntu22_software_install.sh
+++ ubuntu22_software_install.sh
@@ -76,6 +76,65 @@ rm zoom.deb
 # Install xclip
 sudo apt install xclip
 
+### Install ROS 2 Humble desktop full
+# Setup locale
+sudo apt install locales -y
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# Setup sources
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe
+sudo apt update
+
+
+# Add the ROS 2 GPG key
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Add the ROS 2 repository
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2.list'
+
+# Install development tools and ROS tools
+sudo apt update
+
+sudo apt install -y \
+  build-essential \
+  cmake \
+  git \
+  python3-colcon-common-extensions \
+  python3-flake8 \
+  python3-pip \
+  python3-pytest \
+  python3-setuptools \
+  python3-vcstool \
+  wget \
+  libbullet-dev \
+  python3-rosdep \
+  python3-argcomplete \
+  python3-empy \
+  python3-netifaces \
+  python3-yaml
+
+# Install ROS 2 Humble desktop
+sudo apt update
+sudo apt install ros-humble-desktop -y
+
+# Source the setup script
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# Install and initialize rosdep
+sudo apt install python3-rosdep -y
+sudo rosdep init
+rosdep update
+
+echo "ROS 2 Humble Desktop Full installation completed successfully!"
+
+
+
 sudo apt autoremove
 
 echo "Installation of all software completed successfully!"
